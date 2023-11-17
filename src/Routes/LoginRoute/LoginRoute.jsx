bc20d11b630584c389873aa393d371bfcd5faf1a
Khilaar <luca.oliverio@hotmail.com>
2023-11-17 12:44:31 +0100

    started the login logic by saving the email and password from the input field to the state variable and deleted the hello world from app.jsx

--- src/Routes/LoginRoute/LoginRoute.jsx
+++ src/Routes/LoginRoute/LoginRoute.jsx
@@ -1,13 +1,37 @@
 import CounterTest from "../../Components/CounterTest/CounterTest";
+import { useState } from "react";
 
 const Login = () => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault()
+        console.log("email", email)
+        console.log("password", password)
+    }
+
+
     return (
         <div>
             <h1>Login Page</h1>
             <CounterTest/>
-            <form>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+            <form onSubmit={(e) => handleLoginSubmit(e)}>
+
+                <input name="email" 
+                type="email" 
+                placeholder="Email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}/>
+
+                <input name="password" 
+                type="password" 
+                placeholder="Password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}/>
+
+                <button type="submit">Log in</button>
             </form>
         </div>
     );
