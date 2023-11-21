31d2fd19ee837b1bee9d0d5aa733c01f57f85f94
Khilaar <luca.oliverio@hotmail.com>
2023-11-21 13:51:32 +0100

    created a basic header component for the posts route

--- src/Routes/PostsRoute/PostsRoute.jsx
+++ src/Routes/PostsRoute/PostsRoute.jsx
@@ -1,24 +1,7 @@
-import {
-  StyledAPostsRoute,
-  StyledHeaderPostsRoute,
-  StyledSpanPostsRoute,
-} from "../../Styles/PostsRouteStyles";
+import PostsRouteHeader from "../../Components/PostsRouteHeader/PostsRouteHeader";
 
 const Posts = () => {
-  return (
-    <div>
-      <StyledHeaderPostsRoute>
-        <input type="text" placeholder="Search posts" />
-        <StyledSpanPostsRoute>
-          <StyledAPostsRoute href="#">All</StyledAPostsRoute>
-          <StyledAPostsRoute href="#">Liked</StyledAPostsRoute>
-          <StyledAPostsRoute href="#">Friends</StyledAPostsRoute>
-          <StyledAPostsRoute href="#">Follow</StyledAPostsRoute>
-        </StyledSpanPostsRoute>
-      </StyledHeaderPostsRoute>
-      <h2>Posts</h2>
-    </div>
-  );
+  return <PostsRouteHeader />;
 };
 
 export default Posts;
