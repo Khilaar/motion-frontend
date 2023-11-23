43cc585beb18bfa974b58b94b6736b3a5413951e
Khilaar <luca.oliverio@hotmail.com>
2023-11-23 18:13:46 +0100

    Added the functionality to post something. Only text yet

--- src/Components/Post/AddPost.jsx
+++ src/Components/Post/AddPost.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addPost } from "../../Store/Slices/postsSLice";
+import { api } from "../../API/api";
 import { StyledDivAddPost } from "../../Styles/PostsRouteStyles";
 
 function AddPost() {
+  const dispatch = useDispatch();
+  const [postContent, setPostContent] = useState("");
+
+  const handlePost = async () => {
+    try {
+      const token = localStorage.getItem("accessToken");
+      const response = await api.post(
+        "/social/posts/",
+        { content: postContent },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      dispatch(addPost(response.data));
+      setPostContent("");
+    } catch (error) {
+      console.log("Error creating post:", error);
+    }
+  };
+
   return (
     <StyledDivAddPost>
       <div>
         <img src="../src/Assets/pb_placeholder.jpg" alt="" />
-        <input type="text" placeholder="Whats on your mind?" />
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          value={postContent}
+          onChange={(e) => setPostContent(e.target.value)}
+        />
       </div>
-      <button></button>
+      <button onClick={handlePost}>Post</button>
     </StyledDivAddPost>
   );
 }
