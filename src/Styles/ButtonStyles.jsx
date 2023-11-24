30d7df18df389512e3dd8ad2eb40caae5215068e
Shad Sachikonye <shadreck.sachikonye@gmail.com>
2023-11-24 23:38:12 +0000

    add profile card and conditional rendering

--- src/Styles/ButtonStyles.jsx
+++ src/Styles/ButtonStyles.jsx
@@ -13,3 +13,17 @@ export const StyledBtn = styled.button`
     cursor: pointer;
   }
 `;
+
+export const StyledClearButton = styled.button`
+  width: 150px;
+  height: 30px;
+  font-family: roboto;
+  font-weight: 400;
+  font-size: 10px;
+  text-align: center;
+  background-color: white;
+  border-radius: 24px;
+  border: solid 1px rgba(0, 0, 0, 0.2);
+  text-transform: uppercase;
+  opacity: 80%;
+`;
